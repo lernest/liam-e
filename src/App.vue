564750ab3e21c1b441306356800f3007c1b15cec
lernest <19194046+lernest@users.noreply.github.com>
2022-12-26 12:22:32 -0800

    restructure homepage

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/juggler">Juggler</router-link>
+      <router-link to="/juggler">Juggler</router-link> | 
+      <a href="https://github.com/lernest">GitHub</a>
     </div>
     <div class="container">
     </div>
@@ -18,11 +19,44 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Oxygen&family=Oxygen+Mono&display=swap');
+
+:root{
+  --primary-bkg-color: #171321;
+  --secondary-bkg-color: #11334A;
+  --tertiary-bkg-color: #4B0D49;
+  --accent-color: #E210CB;
+  --light-accent-color: #86FBFB;
+  --font-size: 1.3rem;
+  --sans: 'Oxygen', sans-serif;
+  --mono:'Oxygen Mono', monospace;
+}
+
+
+html{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit
+}
+
+body{
+  padding: 0;
+  margin: 0;
+  font-family: var(--sans)
+}
+
+/* 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--secondary-font);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  : center;
   color: #2c3e50;
 }
 
@@ -37,5 +71,5 @@ export default {
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/juggler">Juggler</router-link> | 
+      <a href="https://github.com/lernest">GitHub</a>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
